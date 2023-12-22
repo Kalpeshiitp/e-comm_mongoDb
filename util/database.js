11d7abf1214d706f141c4fc3bce9098438258f97
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-12-22 17:16:02 +0530

    first commit

---
+++ util/database.js
@@ -0,0 +1,31 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://kalpeshkumar267:OGWuzEWGmTX4qHcI@cluster0.h9f3znc.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+const getDb = ()=>{
+  if(_db){
+    return _db
+  } else{
+    throw "no data found"
+  }
+}
+
+module.exports = {
+  mongoConnect: mongoConnect,
+  getDb: getDb
+};
