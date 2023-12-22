a4269a34f84da5830155d8b62ec7b65dc7b76eb2
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-12-23 00:50:40 +0530

    edit, delete product

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://kalpeshkumar267:OGWuzEWGmTX4qHcI@cluster0.h9f3znc.mongodb.net/shop?retryWrites=true&w=majority'
+    'mongodb+srv://kalpeshkumar267:mernChatApp@cluster0.8cmjxlo.mongodb.net/shop?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
