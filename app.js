0b405b27eab5a76f5179a3bf05ff465999effd63
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-12-24 13:31:06 +0530

    increase the product quantity

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('658661a92481c37c59b95d4f')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name,user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
