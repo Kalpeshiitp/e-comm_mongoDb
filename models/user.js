e9ce6cfbf83a7a8a4c970064edf00b8c8219a7f4
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-12-25 16:37:05 +0530

    get orders

--- models/user.js
+++ models/user.js
@@ -57,6 +57,11 @@ userSchema.methods.removeFromCart = function(productId){
 }
 
 
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
   // userSchema.method.getCart= function(products) {
   //   const productIds = this.cart.items.map((i) => {
